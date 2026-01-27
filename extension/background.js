84113046168314ea68629063517d3d6f19e99faf
Sana Yasmine <yasminesana273@gmail.com>
2026-01-27 16:04:46 +0530

    Fix Gmail-safe banner injection using background service worker

--- extension/background.js
+++ extension/background.js
@@ -1,13 +1,26 @@
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === "ANALYZE_URL") {
-    fetch("http://127.0.0.1:8000/analyze", {
+const BACKEND_URL = "https://phishguard-backend-upyk.onrender.com/analyze";
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "ANALYZE_URL") {
+    fetch(BACKEND_URL, {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ url: request.url })
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ url: message.url })
     })
       .then(res => res.json())
-      .then(data => sendResponse({ success: true, data }))
-      .catch(err => sendResponse({ success: false, error: err.toString() }));
+      .then(data => {
+        sendResponse({ success: true, data });
+      })
+      .catch(err => {
+        sendResponse({
+          success: false,
+          error: err.toString()
+        });
+      });
+
+    // 🔴 REQUIRED: async response
     return true;
   }
 });
